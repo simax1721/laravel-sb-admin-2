2838b59af686476e6cbe12e2f9bfca8fc78101b6
simax1721 <simax1721@gmail.com>
2024-02-08 23:02:56 +0700

    repair

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,14 +13,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ url('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ url('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
     <!-- Favicon -->
-    <link href="{{ asset('img/favicon.png') }}" rel="icon" type="image/png">
+    <link href="{{ url('img/favicon.png') }}" rel="icon" type="image/png">
 </head>
 <body id="page-top">
 
@@ -325,9 +325,9 @@
 </div>
 
 <!-- Scripts -->
-<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+<script src="{{ url('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ url('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ url('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="{{ url('js/sb-admin-2.min.js') }}"></script>
 </body>
 </html>
